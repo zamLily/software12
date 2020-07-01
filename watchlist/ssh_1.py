5ddb1e14cf850a6f2b8be587f7b2b3dae2fe6a08
Misaka-hx <57315133+Misaka-hx@users.noreply.github.com>
2020-07-01 15:24:37 +0800

    Add files via upload

--- watchlist/ssh_1.py
+++ watchlist/ssh_1.py
@@ -53,34 +53,28 @@ class DockerApi():
     """
     docker class
     """
-    def __init__(self, ssh_client, student):
+    def __init__(self, ssh_client):
         if not isinstance(ssh_client, SSHManager):
             raise RuntimeError('ssh_client is not SSHManager object')
         self.ssh_client = ssh_client
-        self.student = student
-
-    # 保证为用户新建container的时候没有同名的container
-    def pre_delete_container(self):
-        rm_cmd = 'sudo docker rm -v {}'.format(self.student)
-        # print(rm_cmd)
-        self.ssh_client.ssh_exec_cmd(rm_cmd)
 
     def run_container(self, gpu_num, image_name, file_name):
         # self.ssh_client.ssh_exec_cmd('cd /home/dc2-user')
 
         # run a container
-        run_cmd = 'sudo docker run -itd --gpus={0} --name={1} -v $PWD/code/:/data -w /data {2} python {3}'.format(
-            gpu_num, self.student, image_name, file_name)
-        # print(run_cmd)
-        self.ssh_client.ssh_exec_cmd(run_cmd)
+        run_cmd = 'sudo docker run -itd --gpus={0} -v $PWD/code/:/data -w /data {1} python {2}'.format(
+            gpu_num, image_name, file_name)
+        print(run_cmd)
+        container = self.ssh_client.ssh_exec_cmd(run_cmd)
+        self.container_id = container[:12]
         # print("running the container...")
 
-        log_cmd = 'sudo docker logs {0}'.format(self.student)
-        rm_cmd = 'sudo docker rm -v {}'.format(self.student)
+        log_cmd = 'sudo docker logs {0}'.format(self.container_id)
+        rm_cmd = 'sudo docker rm -v {}'.format(self.container_id)
 
         # return the container log and delete the container
         while True:
-            status = self.ssh_client.ssh_exec_cmd('sudo docker ps | grep {0} | wc -l'.format(self.student))
+            status = self.ssh_client.ssh_exec_cmd('sudo docker ps | grep {0} | wc -l'.format(self.container_id))
             # print("status is {}".format(int(status)))
             if int(status) == 0:
                 # print("saving now")
@@ -90,10 +84,9 @@ class DockerApi():
         return log
 
 
-def docker_test(file_name, ip, port, password, gpu_user, gpu_num, student_id):
+def docker_test(file_name, ip, port, password, gpu_user, gpu_num):
     clinet = SSHManager(ip, port, gpu_user, password)
-    api = DockerApi(clinet, student_id)
-    api.pre_delete_container()
+    api = DockerApi(clinet)
     result = api.run_container(gpu_num, 'tensorflow/tensorflow:latest-gpu', file_name)
     return result
 
@@ -104,18 +97,17 @@ for i in range(num):
     free_gpu_list.append(i)
 gpu = None
 
-file_name = '11.py'
+file_name = 'mnist1.py'
 ip = '116.85.38.198'
 port = 22
 gpu_user = 'dc2-user'
 password = 'Hx1021$&@'
-stu = '1111'
 
 # 获取空闲的gpu
 if free_gpu_list:
     gpu = free_gpu_list.pop(0)
     # print(gpu)
-    res = docker_test(file_name, ip, port, password, gpu_user, gpu, stu)
+    res = docker_test(file_name, ip, port, password, gpu_user, gpu)
     print(res)
     # 释放占用的gpu
     free_gpu_list.append(gpu)
